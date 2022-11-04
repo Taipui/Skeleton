e16d0dd6577d79ec9be17da3066f543535071879
taipui <taipuiupiat@gmail.com>
2022-11-04 22:15:21 +0900

    chore: アニメーション待機検知処理を追加する

---
+++ Assets/Scenes/AnimationTest/AnimationTest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+namespace TW.Animation.Test
+{
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    /// <summary>
+    /// TW.Animationのテスト.
+    /// </summary>
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    public sealed class AnimationTest : MonoBehaviour
+    {
+        //================================================================================
+        // Fields.
+        //================================================================================
+
+        [SerializeField] private Animator Animator = default;
+
+        //================================================================================
+        // Methods.
+        //================================================================================
+
+        //--------------------------------------------------------------------------------
+        // MonoBehaviour methods.
+        //--------------------------------------------------------------------------------
+
+        private IEnumerator Start()
+        {
+            while (true)
+            {
+                yield return null;
+                yield return new WaitForAnimation(Animator, 0);
+
+                Debug.Log("アニメーション終了");
+            }
+        }
+
+        private void Update()
+        {
+            var animationName = AnimationHelper.GetCurrentAnimationName(Animator);
+            Debug.Log($"現在のアニメーション名: {animationName}");
+        }
+    }
+}
