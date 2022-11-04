9ed834cf93a6219bfbdee990df05241ceedb4f95
taipui <taipuiupiat@gmail.com>
2022-11-04 21:50:00 +0900

    chore: アニメーションのヘルパークラスを追加する

---
+++ Assets/Scripts/TW/Animation/AnimationHelper.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+namespace TW.Animation
+{
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    /// <summary>
+    /// アニメーションのヘルパークラス.
+    /// </summary>
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    public static class AnimationHelper
+    {
+        /// <summary>
+        /// 現在のアニメーション名を返す.
+        /// </summary>
+        /// <param name="animator">Animator.</param>
+        /// <returns>
+        /// アニメーション名.
+        /// 見つからなければstring.Emptyを返す.
+        /// </returns>
+        public static string GetCurrentAnimationName(Animator animator)
+        {
+            if (animator == null)
+            {
+                return string.Empty;
+            }
+
+            var info = animator.GetCurrentAnimatorClipInfo(0);
+            if ((info == null) || (info.Length <= 0))
+            {
+                return string.Empty;
+            }
+
+            var clip = info[0].clip;
+            if (clip == null)
+            {
+                return string.Empty;
+            }
+            
+            return clip.name;
+        }
+    }
+}
