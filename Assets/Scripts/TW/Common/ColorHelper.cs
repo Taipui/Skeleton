ceabdd41bcf140e62a275959e8739fc896a11c58
taipui <taipuiupiat@gmail.com>
2023-03-12 12:10:05 +0900

    chore: ColorHelperを追加する

---
+++ Assets/Scripts/TW/Common/ColorHelper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine.UI;
+
+
+namespace TW.Common
+{
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    /// <summary>
+    /// 色に関するヘルパークラス.
+    /// </summary>
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    public static class ColorHelper
+    {
+        //================================================================================
+        // Methods.
+        //================================================================================
+
+        //--------------------------------------------------------------------------------
+        // Public methods.
+        //--------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Alphaを設定する.
+        /// </summary>
+        /// <param name="image">Image.</param>
+        /// <param name="alpha">アルファ値.</param>
+        public static void SetAlpha(MaskableGraphic image, float alpha)
+        {
+            var color   = image.color;
+            color.a     = alpha;
+            image.color = color;
+        }
+    }
+}
