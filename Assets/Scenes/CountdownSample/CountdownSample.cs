14dcdb2aefb04b3e61c12c844286d7e3c1bf254d
taipui <taipuiupiat@gmail.com>
2023-03-12 12:11:38 +0900

    chore: カウントダウン処理を追加する

---
+++ Assets/Scenes/CountdownSample/CountdownSample.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+namespace TW.Countdown.Test
+{
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    /// <summary>
+    /// Countdownのサンプル.
+    /// </summary>
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    public sealed class CountdownSample : MonoBehaviour
+    {
+        //================================================================================
+        // Fields.
+        //================================================================================
+
+        /// <summary>
+        /// カウントダウンのクラス.
+        /// </summary>
+        [SerializeField] private Applications.Countdown Countdown = default;
+
+        //================================================================================
+        // Methods.
+        //================================================================================
+
+        //--------------------------------------------------------------------------------
+        // MonoBehaviour methods.
+        //--------------------------------------------------------------------------------
+
+        private IEnumerator Start()
+        {
+            yield return Countdown.Do(() => Debug.Log("Countdown is over"));
+        }
+    }
+}
---
+++ Assets/Scripts/TW/Applications/Countdown.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Collections;
+
+using TW.Common;
+
+
+namespace TW.Applications
+{
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    /// <summary>
+    /// カウントダウンする.
+    /// </summary>
+    //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    public sealed class Countdown : MonoBehaviour
+    {
+        //================================================================================
+        // Fields.
+        //================================================================================
+
+        /// <summary>
+        /// カウントダウンのImage.
+        /// </summary>
+        [SerializeField] private Image Image = default;
+
+        /// <summary>
+        /// カウントダウンの画像.
+        /// </summary>
+        /// <remarks>
+        /// 最後の画像はカウントダウン後に表示する画像.
+        /// </remarks>
+        [SerializeField] private Sprite[] Sprites = default;
+
+        /// <summary>
+        /// WaitForSecondsのキャッシュ.
+        /// </summary>
+        private WaitForSeconds Wfs = new(1.0f);
+
+        //================================================================================
+        // Methods.
+        //================================================================================
+
+        //--------------------------------------------------------------------------------
+        // MonoBehaviour methods.
+        //--------------------------------------------------------------------------------
+
+        public void Reset()
+        {
+            ColorHelper.SetAlpha(Image, 0.0f);
+        }
+
+        //--------------------------------------------------------------------------------
+        // Public methods.
+        //--------------------------------------------------------------------------------
+
+        /// <summary>
+        /// カウントダウンするコルーチン.
+        /// </summary>
+        /// <param name="cb">カウントダウンが終わると呼ばれる.</param>
+        public IEnumerator Do(System.Action cb = null)
+        {
+            ColorHelper.SetAlpha(Image, 1.0f);
+            for (var i = 0; i < Sprites.Length - 1; ++i)
+            {
+                Image.overrideSprite = Sprites[i];
+                yield return Wfs;
+            }
+
+            Image.overrideSprite = Sprites[^1];
+            yield return Wfs;
+
+            Reset();
+
+            cb?.Invoke();
+        }
+    }
+}
+
